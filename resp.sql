0209592135811c3a51740f4ff4c9bb946a180292
KaikyVT <67959187+KaikyVT@users.noreply.github.com>
2025-05-08 23:32:07 -0300

    Resposta exercício

---
+++ resp.sql
@@ -0,0 +1,18 @@
+DELIMITER &&
+CREATE
+    FUNCTION nivel_de_conta (saldo DECIMAL(10,2))
+    RETURNS VARCHAR(60)
+    DETERMINISTIC
+    BEGIN
+        IF saldo > 35000 THEN
+            RETURN 'PLATINUM';
+        ELSEIF saldo > 10000 THEN
+            RETURN 'GOLD';
+        ELSE
+            RETURN 'SILVER';
+        END IF;
+    END &&
+DELIMITER ;
+
+select nivel_de_conta(10000)
+from account;
